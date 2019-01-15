b8836ac288b1581ea266707be437454607b5455f
s.weise <s.weise@sw-pc>
2019-01-15 13:12:04 -0800

    1,2,3,4,5,6,7,8 my ages

---
+++ src/ifs_ints_and_loops/MyAges.java
@@ -0,0 +1,15 @@
+package ifs_ints_and_loops;
+
+public class MyAges {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int born = 1978;
+		int year = 2019;
+		
+		for (int i = born; i < year; i++) {
+			System.out.println(i-born);
+		}
+	}
+
+}
